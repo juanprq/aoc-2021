01912b99812868c6b8e2198d6e38d4df2c9fb15a
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2021-12-01 01:04:17 -0500

    adds day 1

--- src/day-1/problem-1.js
+++ src/day-1/problem-1.js
@@ -1,5 +1,13 @@
 const utils = require('../utils');
 
-const data = utils.loadInput(__dirname);
+const data = utils
+  .loadInput(__dirname)
+  .map(v => parseInt(v, 10));
 
-console.log(data);
+let count = 0;
+for (let i = 1; i < data.length; i++) {
+  if (data[i] > data[i - 1]) count++;
+}
+
+console.log('---> result');
+console.log(count);
